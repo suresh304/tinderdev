594f694aeef4ce309eb10965d7b4ea4647e34cf8
suresh304 <sureshallie@gmail.com>
2025-04-30 22:57:14 +0530

    chores

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,11 +3,7 @@ const { mongoose } = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.DB_CONNECTION_STRING, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 20000, // ⬅️ increase from default 10s
-      }
+      process.env.DB_CONNECTION_STRING
     );
     console.log("connected to mongodb");
   } catch (e) {
