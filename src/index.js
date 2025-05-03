1172aaaee5d3c9c993a42e8f5e2eb0d10900da16
suresh304 <sureshallie@gmail.com>
2025-05-03 20:06:00 +0530

    testng2

--- src/index.js
+++ src/index.js
@@ -24,12 +24,12 @@ app.use(cors({
 }))
 
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 // For any unknown routes, serve React index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
 
 
 app.get('/test', (req, res) => {
