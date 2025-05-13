1c3bd954388063230f0b16e077cab9da9cbcecec
suresh304 <sureshallie@gmail.com>
2025-05-13 23:00:01 +0530

    deletemsg

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -70,7 +70,7 @@ const initialiseSocketConnection = (server) => {
                     participants: {
                         $all: [userId, targetUser]
                     }
-                }).populate("messages.senderId", "firstName lastName photoUrl").populate("messages.recieverId", "firstName lastName photoUrl")
+                }).populate("messages.senderId", "firstName lastName photoUrl _id").populate("messages.recieverId", "firstName lastName photoUrl _id")
 
 
                 io.to(room).emit('messagerecieved', chat2.messages.slice(-1)[0])
@@ -91,11 +91,87 @@ const initialiseSocketConnection = (server) => {
             console.log("this is typing--------->", data)
             io.to(room).emit('typingStatusRecieved', { data, senderId: userId, recieverId: targetUser })
         })
-        socket.on("deletingMessage", ({ data, userId, targetUser }) => {
-            const room = [userId, targetUser].sort().join('$')
-            console.log("this is deleting>>--------->", data)
-            io.to(room).emit('messageDeleted', { data, senderId: userId, recieverId: targetUser })
-        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+socket.on("deletingMessage", async ({ msgId, userId, targetUser }) => {
+  try {
+    const room = [userId, targetUser].sort().join('$');
+
+    const chat = await Chat.findOne({
+      participants: { $all: [userId, targetUser] }
+    });
+
+    if (!chat) return;
+
+    // Find the message
+    const msgIndex = chat.messages.findIndex(
+      (msg) => msg._id.toString() === msgId
+    );
+
+    if (msgIndex === -1) return;
+
+    const message = chat.messages[msgIndex];
+
+    // Avoid double delete
+    if (!message.deletedBy.includes(userId)) {
+      message.deletedBy.push(userId);
+    }
+
+    // If both users deleted, remove the message
+    const allDeleted = chat.participants.every(participantId =>
+      message.deletedBy.map(id => id.toString()).includes(participantId.toString())
+    );
+
+    if (allDeleted) {
+      chat.messages.splice(msgIndex, 1);
+    }
+
+    await chat.save();
+
+    // Refetch chat to populate and emit updated messages
+    const updatedChat = await Chat.findOne({
+      participants: { $all: [userId, targetUser] }
+    })
+    .populate("messages.senderId", "firstName lastName photoUrl")
+    .populate("messages.recieverId", "firstName lastName photoUrl");
+
+    // Filter out messages deleted for this user
+    const filteredMessages = updatedChat.messages.filter(
+      (msg) => !msg.deletedBy.includes(userId)
+    );
+
+    io.to(room).emit("messageUpdated", filteredMessages);
+  } catch (err) {
+    console.error("deleteMessage error:", err);
+  }
+});
+
+
+
+
+
+
+
+
+
 
     })
 }
