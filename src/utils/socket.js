5f830912ff728bf7579fc7d5985f50bd8ec93d53
suresh304 <sureshallie@gmail.com>
2025-05-14 21:48:45 +0530

    hello deploy

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,165 +4,148 @@ const { Chat } = require('../models/chat')
 
 const initialiseSocketConnection = (server) => {
 
-    const io = socket(server, {
-        cors: {
-            origin: 'http://localhost:5173',
-            credentials: true
+  const io = socket(server, {
+    cors: {
+      origin: 'http://localhost:5173',
+      credentials: true
 
-        }
+    }
+  })
+
+  io.on("connection", (socket) => {
+
+    // handle events
+
+    socket.on("joinchat", ({ userId, targetUser, firstName }) => {
+      const room = [userId, targetUser].sort().join('$')
+      socket.join(room)
+      console.log(firstName + "join chat")
+
     })
 
-    io.on("connection", (socket) => {
+    socket.on("sendmessage", async ({ firstName, userId, targetUser, message }) => {
 
-        // handle events
+      try {
+        const room = [userId, targetUser].sort().join('$')
 
-        socket.on("joinchat", ({ userId, targetUser, firstName }) => {
-            const room = [userId, targetUser].sort().join('$')
-            socket.join(room)
-            console.log(firstName + "join chat")
+        console.log(firstName + "send message :" + message)
+        console.log(firstName, userId, targetUser, message)
+
+        let chat = await Chat.findOne({
+          participants: {
+            $all: [userId, targetUser]
+          }
+        })
+
+        if (!chat) {
+          chat = new Chat({
+            participants: [userId, targetUser],
+            messages: []
+          })
+        }
+
+        chat.messages.push({
+          senderId: userId,
+          recieverId: targetUser,
+          text: message
 
         })
 
-        socket.on("sendmessage", async ({ firstName, userId, targetUser, message }) => {
+        if (!chat) {
+          chat = new Chat({
+            participants: [userId, targetUser],
+            messages: []
+          })
+        }
 
-            try {
-                const room = [userId, targetUser].sort().join('$')
 
-                console.log(firstName + "send message :" + message)
-                console.log(firstName, userId, targetUser, message)
 
-                let chat = await Chat.findOne({
-                    participants: {
-                        $all: [userId, targetUser]
-                    }
-                })
-
-                if (!chat) {
-                    chat = new Chat({
-                        participants: [userId, targetUser],
-                        messages: []
-                    })
-                }
-
-                chat.messages.push({
-                    senderId: userId,
-                    recieverId: targetUser,
-                    text: message
-
-                })
-
-                if (!chat) {
-                    chat = new Chat({
-                        participants: [userId, targetUser],
-                        messages: []
-                    })
-                }
-
-               
-
-                const data = await chat.save()
+        const data = await chat.save()
 
 
 
 
 
-                let chat2 = await Chat.findOne({
-                    participants: {
-                        $all: [userId, targetUser]
-                    }
-                }).populate("messages.senderId", "firstName lastName photoUrl _id").populate("messages.recieverId", "firstName lastName photoUrl _id")
+        let chat2 = await Chat.findOne({
+          participants: {
+            $all: [userId, targetUser]
+          }
+        }).populate("messages.senderId", "firstName lastName photoUrl _id").populate("messages.recieverId", "firstName lastName photoUrl _id")
 
 
-                io.to(room).emit('messagerecieved', chat2.messages.slice(-1)[0])
-            } catch (error) {
+        io.to(room).emit('messagerecieved', chat2.messages.slice(-1)[0])
+      } catch (error) {
 
-            }
-
-
+      }
 
 
+
+
+    })
+    socket.on("disconnect", () => {
+
+    })
+
+    socket.on("typing", ({ data, userId, targetUser }) => {
+      const room = [userId, targetUser].sort().join('$')
+      console.log("this is typing--------->", data)
+      io.to(room).emit('typingStatusRecieved', { data, senderId: userId, recieverId: targetUser })
+    })
+
+
+    socket.on("deletingMessage", async ({ msgId, userId, targetUser }) => {
+      try {
+        const room = [userId, targetUser].sort().join('$');
+
+        const chat = await Chat.findOne({
+          participants: { $all: [userId, targetUser] }
+        });
+
+        if (!chat) return;
+
+        // Find the message
+        const msgIndex = chat.messages.findIndex(
+          (msg) => msg._id.toString() === msgId
+        );
+
+        if (msgIndex === -1) return;
+
+        const message = chat.messages[msgIndex];
+
+        // Avoid double delete
+        if (!message.deletedBy.includes(userId)) {
+          message.deletedBy.push(userId);
+        }
+
+        // If both users deleted, remove the message
+        const allDeleted = chat.participants.every(participantId =>
+          message.deletedBy.map(id => id.toString()).includes(participantId.toString())
+        );
+
+        if (allDeleted) {
+          chat.messages.splice(msgIndex, 1);
+        }
+
+        await chat.save();
+
+        // Refetch chat to populate and emit updated messages
+        const updatedChat = await Chat.findOne({
+          participants: { $all: [userId, targetUser] }
         })
-        socket.on("disconnect", () => {
+          .populate("messages.senderId", "firstName lastName photoUrl")
+          .populate("messages.recieverId", "firstName lastName photoUrl");
 
-        })
+        // Filter out messages deleted for this user
+        const filteredMessages = updatedChat.messages.filter(
+          (msg) => !msg.deletedBy.includes(userId)
+        );
 
-        socket.on("typing", ({ data, userId, targetUser }) => {
-            const room = [userId, targetUser].sort().join('$')
-            console.log("this is typing--------->", data)
-            io.to(room).emit('typingStatusRecieved', { data, senderId: userId, recieverId: targetUser })
-        })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-socket.on("deletingMessage", async ({ msgId, userId, targetUser }) => {
-  try {
-    const room = [userId, targetUser].sort().join('$');
-
-    const chat = await Chat.findOne({
-      participants: { $all: [userId, targetUser] }
+        io.to(room).emit("messageUpdated", filteredMessages);
+      } catch (err) {
+        console.error("deleteMessage error:", err);
+      }
     });
 
-    if (!chat) return;
-
-    // Find the message
-    const msgIndex = chat.messages.findIndex(
-      (msg) => msg._id.toString() === msgId
-    );
-
-    if (msgIndex === -1) return;
-
-    const message = chat.messages[msgIndex];
-
-    // Avoid double delete
-    if (!message.deletedBy.includes(userId)) {
-      message.deletedBy.push(userId);
-    }
-
-    // If both users deleted, remove the message
-    const allDeleted = chat.participants.every(participantId =>
-      message.deletedBy.map(id => id.toString()).includes(participantId.toString())
-    );
-
-    if (allDeleted) {
-      chat.messages.splice(msgIndex, 1);
-    }
-
-    await chat.save();
-
-    // Refetch chat to populate and emit updated messages
-    const updatedChat = await Chat.findOne({
-      participants: { $all: [userId, targetUser] }
-    })
-    .populate("messages.senderId", "firstName lastName photoUrl")
-    .populate("messages.recieverId", "firstName lastName photoUrl");
-
-    // Filter out messages deleted for this user
-    const filteredMessages = updatedChat.messages.filter(
-      (msg) => !msg.deletedBy.includes(userId)
-    );
-
-    io.to(room).emit("messageUpdated", filteredMessages);
-  } catch (err) {
-    console.error("deleteMessage error:", err);
-  }
-});
 
 
 
@@ -172,8 +155,7 @@ socket.on("deletingMessage", async ({ msgId, userId, targetUser }) => {
 
 
 
-
-    })
+  })
 }
 
 module.exports = { initialiseSocketConnection }
